ea86c833dc2e561527b7c5922835a18a27791919
Akshata <akshatha2022@gmail.com>
2024-06-05 20:41:25 +0530

    Updated IP to use production mode

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def upload_file():
         return render_template('prediction.html', prediction_results=prediction_results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)  Local
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))     #Production mode
